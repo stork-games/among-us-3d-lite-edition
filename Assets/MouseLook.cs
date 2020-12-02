837d27975e0d7ffa3c562723d771785d88f2b4d8
c12games <coolstone1337@ya.ru>
2020-12-02 21:19:19 +0300

    лол
    лол

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Чувствительность мышки по двум осям
+    [SerializeField] private float sensitivityX = 1.0f, sensitivityY = 1.0f;
+
+    // Минимальное и максимальное вращение по оси X
+    [SerializeField] private float minX = -90.0f, maxX = 90.0f;
+
+    // Опции поворота
+    private enum Options { X, Y, XandY }
+
+    // Выбранная опция поворота
+    [SerializeField] private Options options;
+
+    // Текущий поворот
+    private Quaternion targetRot;
+
+    void Start()
+    {
+        // Сохраняем текущий поворот
+        targetRot = transform.rotation;
+    }
+
+    void Update()
+    {
+        // Получаем значения поворота мыши и умножаем на чувствительность поворота
+        float rotY = Input.GetAxis("Mouse X") * sensitivityX;
+        float rotX = Input.GetAxis("Mouse Y") * sensitivityY;
+
+        // Определяем необходимую ось вращения
+        if (options == Options.X)
+            // Устанавливаем текущий поворот на необходимый угол
+            targetRot *= Quaternion.Euler(-rotX, 0.0f, 0.0f);
+        else if (options == Options.Y)
+            targetRot *= Quaternion.Euler(0.0f, rotY, 0.0f);
+        else if (options == Options.XandY)
+            targetRot *= Quaternion.Euler(-rotX, rotY, 0.0f);
+
+        // Поворачиваем объект
+        transform.localRotation = targetRot;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Скорость передвижения игрока
+    [SerializeField] private float speed = 10.0f;
+
+    // Компонент CharacterController
+    private CharacterController cc;
+    Animator animator;
+
+    void Start()
+    {
+        // Получаем компонент CharacterController
+        cc = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        // Получаем нажатия предустановленных клавиш
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        animator.SetBool("IsWalking", isWalking);
+
+        // Записываем данные в отдельную переменную
+        Vector2 input = new Vector2(horizontal, vertical);
+        // Определяем направление движения
+        Vector3 desiredMove = transform.forward * input.y + transform.right * input.x;
+        Vector3 moveDir = new Vector3(desiredMove.x * speed, 0, desiredMove.z * speed);
+        // Передвигаем объект
+        cc.Move(moveDir * Time.fixedDeltaTime);
+    }
+}
